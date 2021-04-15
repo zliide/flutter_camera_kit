aa46ac5b7f64b2ef2c84940e9529f5a4f1e98976
Marek Cervinka <marek@zliide.com>
2021-04-15 19:23:00 +0200

    Dispose previously assigned view

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -3,6 +3,7 @@ import 'package:camerakit/src/view.dart';
 ///This controller is used to control CameraKiView.dart
 class CameraKitController {
   late CameraKitView cameraKitView;
+  bool _initialized = false;
 
   ///pause camera while stop camera preview.
   ///Plugin manage automatically pause camera based android, iOS lifecycle and widget visibility
@@ -34,6 +35,10 @@ class CameraKitController {
 
   ///Connect view to this controller
   void setView(CameraKitView cameraKitView) {
+    if (_initialized) {
+      dispose();
+    }
     this.cameraKitView = cameraKitView;
+    _initialized = true;
   }
 }
